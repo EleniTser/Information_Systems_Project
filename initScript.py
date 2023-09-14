20b28093493b96e60e0d2a67dcdd139eed385da8
EleniTser <81263938+EleniTser@users.noreply.github.com>
2023-09-14 11:13:45 +0300

    Add files via upload

---
+++ initScript.py
@@ -0,0 +1,142 @@
+from cassandra.cluster import Cluster
+
+# Connect to Cassandra
+cluster = Cluster(['192.168.0.2']) 
+session = cluster.connect()
+
+# Create the keyspace
+session.execute("CREATE KEYSPACE IF NOT EXISTS mykeyspace WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '2'}")
+
+# Create the table
+session.execute("""
+    CREATE TABLE IF NOT EXISTS mykeyspace.cputable (
+        series_text TEXT,
+        metric_text TEXT,
+        hostname TEXT,
+        region TEXT,
+        datacenter TEXT,
+        rack TEXT,
+        os TEXT,
+        arch TEXT,
+        team TEXT,
+        service TEXT,
+        service_version TEXT,
+        service_environment TEXT,
+        usage_type TEXT,
+        date DATE,
+        timestamp BIGINT,
+        value BIGINT,
+        PRIMARY KEY ((hostname, region, datacenter, rack, os, arch, team, service, service_version, service_environment, usage_type, date), timestamp)
+    )
+""")
+
+session.execute("""
+    CREATE TABLE IF NOT EXISTS mykeyspace.query_1_table (
+        hostname TEXT,
+        usage_type TEXT,
+        timestamp BIGINT,
+        value BIGINT,
+        PRIMARY KEY (usage_type, timestamp, hostname)
+    )
+    WITH CLUSTERING ORDER BY (timestamp ASC)
+""")
+
+session.execute("""
+    CREATE TABLE IF NOT EXISTS mykeyspace.query_2_table (
+        hostname TEXT,
+        usage_type TEXT,
+        timestamp BIGINT,
+        value BIGINT,
+        PRIMARY KEY (hostname, timestamp, usage_type)
+    )
+    WITH CLUSTERING ORDER BY (timestamp ASC)
+""")
+
+session.execute("""
+    CREATE TABLE IF NOT EXISTS mykeyspace.query_3_table (
+        hostname TEXT,
+        usage_type TEXT,
+        timestamp BIGINT,
+        value BIGINT,
+        PRIMARY KEY (usage_type, hostname)
+    )
+""")
+
+session.execute("""
+    CREATE TABLE IF NOT EXISTS mykeyspace.query_4_table (
+        hostname TEXT,
+        usage_type TEXT,
+        timestamp BIGINT,
+        value BIGINT,
+        PRIMARY KEY ((hostname, usage_type), timestamp)
+    )
+""")
+
+session.execute("""
+    CREATE TABLE IF NOT EXISTS mykeyspace.query_5_table (
+        hostname TEXT,
+        usage_type TEXT,
+        timestamp BIGINT,
+        value BIGINT,
+        PRIMARY KEY (usage_type, hostname, timestamp)
+    )
+""")
+
+session.execute("""
+    CREATE TABLE IF NOT EXISTS mykeyspace.query_6_table (
+        hostname TEXT,
+        usage_type TEXT,
+        timestamp BIGINT,
+        value BIGINT,
+        PRIMARY KEY (usage_type, hostname, timestamp)
+    )
+""")
+
+session.execute("CREATE INDEX IF NOT EXISTS hostname_index ON mykeyspace.query_6_table(hostname)")
+
+session.execute("""
+    CREATE TABLE IF NOT EXISTS mykeyspace.query_7_table (
+        datacenter TEXT,
+        usage_type TEXT,
+	timestamp BIGINT,
+        value BIGINT,
+        PRIMARY KEY ((datacenter, usage_type),timestamp)
+    )
+""")
+
+session.execute("""
+    CREATE TABLE IF NOT EXISTS mykeyspace.query_8_table (
+        hostname TEXT,
+        datacenter TEXT,
+        usage_type TEXT,
+        timestamp BIGINT,
+        value BIGINT,
+        PRIMARY KEY (datacenter, timestamp, usage_type, hostname)
+    )
+    WITH CLUSTERING ORDER BY (timestamp ASC)
+""")
+
+session.execute("""
+    CREATE TABLE IF NOT EXISTS mykeyspace.query_9_table (
+        datacenter TEXT,
+        usage_type TEXT,
+        timestamp BIGINT,
+        value BIGINT,
+        PRIMARY KEY (datacenter, timestamp, usage_type)
+    )
+    WITH CLUSTERING ORDER BY (timestamp ASC)
+""")
+
+session.execute("""
+    CREATE TABLE IF NOT EXISTS mykeyspace.query_10_table (
+        hostname TEXT,
+        datacenter TEXT,
+        service TEXT,
+        usage_type TEXT,
+        timestamp BIGINT,
+        value BIGINT,
+        PRIMARY KEY ((datacenter, usage_type), hostname, service, timestamp)
+    )
+""")
+
+session.execute("CREATE INDEX IF NOT EXISTS service_index ON mykeyspace.query_10_table(service)")
---
+++ loadCSVWithdsbulk.py
@@ -0,0 +1,116 @@
+import os
+import time
+import subprocess
+
+# Set the paths and options
+dsbulk_path = "/home/user/info_sys/cassandra/dsbulk-1.11.0/bin/dsbulk"  # Path to the DSBulk executable
+keyspace = "mykeyspace"
+
+tables = ["query_{}_table".format(i) for i in range(1, 11)] 
+
+#csv_file = "/mnt/f/dummyData/sampleData_1000000.csv"  # Replace with your CSV file name
+#csv_file = "/mnt/c/dummyData/small-sample-data-cassandra.csv"
+csv_file_1 = "/home/user/info_sys/cassandra/csv_file_1"
+csv_file_2 = "/home/user/info_sys/cassandra/csv_file_2"
+csv_file_3 = "/home/user/info_sys/cassandra/csv_file_3"
+csv_file_4 = "/home/user/info_sys/cassandra/csv_file_4"
+
+time_list = {}
+for table in ["query_1_table", "query_2_table", "query_3_table", "query_4_table", "query_5_table", "query_6_table"]: 
+    # Define the DSBulk command
+    start_time = time.time()
+    dsbulk_cmd = [
+        dsbulk_path,
+        "load",
+        "-h", "192.168.0.2",
+        "-k", keyspace,
+        "-t", table,
+        "-url", csv_file_1,
+        "-header", "false",
+        "-delim", ",",  # Replace with the delimiter used in your CSV
+        "-m", "0=hostname, 1=usage_type, 2=timestamp, 3=value"
+        ]
+
+    # Execute the DSBulk command
+    try:
+        subprocess.run(dsbulk_cmd, check=True)
+        print("CSV data loaded successfully using DSBulk.")
+    except subprocess.CalledProcessError as e:
+        print("Error:", e)
+    end_time = time.time()
+    time_list[table] = end_time - start_time
+ 
+for table in ["query_7_table", "query_9_table"]:
+    # Define the DSBulk command
+    start_time = time.time()
+    dsbulk_cmd = [
+        dsbulk_path,
+        "load",
+        "-h", "192.168.0.2",
+        "-k", keyspace,
+        "-t", table,
+        "-url", csv_file_2,
+        "-header", "false",
+        "-delim", ",",  # Replace with the delimiter used in your CSV
+        "-m", "0=datacenter, 1=usage_type, 2=timestamp, 3=value"
+        ]
+
+    # Execute the DSBulk command
+    try:
+        subprocess.run(dsbulk_cmd, check=True)
+        print("CSV data loaded successfully using DSBulk.")
+    except subprocess.CalledProcessError as e:
+        print("Error:", e)
+    end_time = time.time()
+    time_list[table] = end_time - start_time
+
+table = "query_8_table"
+start_time = time.time()
+# Define the DSBulk command
+dsbulk_cmd = [
+    dsbulk_path,
+    "load",
+    "-h", "192.168.0.2",
+    "-k", keyspace,
+    "-t", table,
+    "-url", csv_file_3,
+    "-header", "false",
+    "-delim", ",",  # Replace with the delimiter used in your CSV
+    "-m", "0=hostname, 1=datacenter, 2=usage_type, 3=timestamp, 4=value"
+    ]
+
+# Execute the DSBulk command
+try:
+    subprocess.run(dsbulk_cmd, check=True)
+    print("CSV data loaded successfully using DSBulk.")
+except subprocess.CalledProcessError as e:
+    print("Error:", e)
+end_time = time.time()
+time_list[table] = end_time - start_time
+
+
+table = "query_10_table"
+start_time = time.time()
+# Define the DSBulk command
+dsbulk_cmd = [
+    dsbulk_path,
+    "load",
+    "-h", "192.168.0.2",
+    "-k", keyspace,
+    "-t", table,
+    "-url", csv_file_4,
+    "-header", "false",
+    "-delim", ",",  # Replace with the delimiter used in your CSV
+    "-m", "0=hostname, 1=datacenter, 2=service, 3=usage_type, 4=timestamp, 5=value"
+    ]
+
+# Execute the DSBulk command
+try:
+    subprocess.run(dsbulk_cmd, check=True)
+    print("CSV data loaded successfully using DSBulk.")
+except subprocess.CalledProcessError as e:
+    print("Error:", e)
+end_time = time.time()
+time_list[table] = end_time - start_time
+
+print(time_list)
